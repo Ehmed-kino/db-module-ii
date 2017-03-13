6306eaac2267b28eeaea72bdb64cea1ccf119545
ehmed-kino <ehmedaff@gmail.com>
2017-03-13 23:20:35 +0000

    solve the get problem

--- src/routes/organisations.js
+++ src/routes/organisations.js
@@ -4,11 +4,43 @@ const db = require('sqlite');
 
 module.exports = {
     get(req, res) {
-        res.status(404).json({ error: 'Not found' });
+      let organisations;
+
+        db.all(`SELECT * FROM organisation`)
+        .then((orgs) => {
+          organisations = orgs;
+          return Promise.all(orgs.map((org) => {
+            return db.all(`SELECT Service.Id,Service.Name FROM service JOIN organisationService
+                    ON service.Id = organisationService.serviceId
+                    WHERE organisationService.organisationId = ${org.Id}`)
+          }))
+        })
+        .then((services) => {
+          for(let i=0; i< organisations.length; i++) {
+             organisations[i].Services = services[i];
+          }
+
+        })
+        .then(() => {
+          return Promise.all(organisations.map((org) => {
+            return db.all(`SELECT Organisation.Name, user.Name, user.Email FROM
+                           User JOIN Role JOIN ContactUserOrganisation JOIN Organisation
+                           ON User.Id = ContactUserOrganisation.UserId
+                           AND Organisation.Id = ContactUserOrganisation.OrganisationId
+                           WHERE Organisation.Id = ${org.Id} AND Role.Id = 2`)
+          }))
+        }).then((contacts) => {
+          for(let i=0; i< organisations.length; i++) {
+             organisations[i].Contacts = contacts[i];
+          }
+
+        }).then(() => {
+          res.json(organisations)
+        })
     },
 
     add(req, res) {
-        res.status(404).json({ error: 'Not found' });
+        
     },
 
     update(req, res) {
